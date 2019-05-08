27bf80bb40c2062fedeb0177306c8624e803599f
Roman Baskakov <43024078+RBaskakov@users.noreply.github.com>
2019-05-08 11:28:44 +0700

    Update TaskTest.cs

--- TaskTest/TaskTest.cs
+++ TaskTest/TaskTest.cs
@@ -11,78 +11,96 @@ namespace TaskTest
         public void TestEmployeeSalary()
         {
             // arrange
-            string Name = "Employee 1";
             decimal BaseSalary = 200M;
             DateTime DateJoined = new DateTime(2013, 1, 1);
             decimal EхpectedSalary = 236M;
 
             //act
-            Employee e = new Employee(Name, BaseSalary, DateJoined);
+            Employee e = new Employee("Employee", BaseSalary, DateJoined);
             decimal ActualSalary = e.Salary;
             // assert
-            if (e.ErrorMessage != "") { Assert.Fail(e.ErrorMessage); }            
-            Assert.AreEqual(EхpectedSalary, ActualSalary);
+            if (e.ErrorMessage != "") { 
+                Assert.Fail(e.ErrorMessage);
+            }
+            else {
+                Assert.AreEqual(EхpectedSalary, ActualSalary);
+            }            
             
         }
         [TestMethod]
         public void TestEmployeeLongWorkSalary()
         {
             // arrange
-            string Name = "Employee long work";
             decimal BaseSalary = 200M;
             DateTime DateJoined = new DateTime(2003, 1, 1);
             decimal EхpectedSalary = 260M;
 
             //act
 
-            Employee e = new Employee(Name, BaseSalary, DateJoined);
+            Employee e = new Employee("Employee long work", BaseSalary, DateJoined);
             decimal ActualSalary = e.Salary;
 
             // assert
-            if (e.ErrorMessage != "") { Assert.Fail(e.ErrorMessage); }
-            Assert.AreEqual(EхpectedSalary, ActualSalary);
+            if (e.ErrorMessage != "")
+            {
+                Assert.Fail(e.ErrorMessage);
+            }
+            else
+            {
+                Assert.AreEqual(EхpectedSalary, ActualSalary);
+            }            
         }
         [TestMethod]
         public void TestManagerSalaryWithoutSubordinates()
         {
             // arrange
-            string Name = "Manager Without Subordinates";
             decimal BaseSalary = 300M;
             DateTime DateJoined = new DateTime(2014, 1, 1);
             decimal EхpectedSalary = 375M;
 
             //act
 
-            Manager m = new Manager(Name, BaseSalary, DateJoined);
+            Manager m = new Manager("Manager Without Subordinates", BaseSalary, DateJoined);
             decimal ActualSalary = m.Salary;
 
             // assert
-            if (m.ErrorMessage != "") { Assert.Fail(m.ErrorMessage); }
-            Assert.AreEqual(EхpectedSalary, ActualSalary);
+            if (m.ErrorMessage != "")
+            {
+                Assert.Fail(m.ErrorMessage);
+            }
+            else
+            {
+                Assert.AreEqual(EхpectedSalary, ActualSalary);
+            }            
         }
         [TestMethod]
         public void TestManagerLongWorkSalaryWitoutSubordinates()
         {
             // arrange
-            string Name = "Manager long work";
             decimal BaseSalary = 300M;
             DateTime DateJoined = new DateTime(2004, 1, 1);
             decimal EхpectedSalary = 420M;
 
             //act
 
-            Manager m = new Manager(Name, BaseSalary, DateJoined);
+            Manager m = new Manager("Manager long work", BaseSalary, DateJoined);
             decimal ActualSalary = m.Salary;
 
             // assert
-            if (m.ErrorMessage != "") { Assert.Fail(m.ErrorMessage); }
-            Assert.AreEqual(EхpectedSalary, ActualSalary);
+            if (m.ErrorMessage != "")
+            {
+                Assert.Fail(m.ErrorMessage);
+            }
+            else
+            {
+                Assert.AreEqual(EхpectedSalary, ActualSalary);
+            }            
         }
         [TestMethod]
         public void TestManagerSalaryWithSubordinates()
         {
             // arrange
-            string Name = "Manager with Subordinates";
+            string TestErrorMessage="";
             decimal BaseSalaryManager = 300M;
             decimal BaseSalaryEmployee1 = 100M;
             decimal BaseSalaryEmployee2 = 160M;
@@ -91,56 +109,64 @@ namespace TaskTest
 
             //act
 
-            Manager m = new Manager(Name, BaseSalaryManager, DateJoined);
-            Employee e1 = new Employee(Name, BaseSalaryEmployee1, DateJoined);
+            Manager m = new Manager("Manager with Subordinates", BaseSalaryManager, DateJoined);
+            Employee e1 = new Employee("Employee 1", BaseSalaryEmployee1, DateJoined);
             e1.Supervisor = m;
-            Employee e2 = new Employee(Name, BaseSalaryEmployee2, DateJoined);
+            Employee e2 = new Employee("Employee 2", BaseSalaryEmployee2, DateJoined);
             e2.Supervisor = m;
             
             decimal ActualSalaryManager = m.Salary;
 
             // assert
-            if (m.ErrorMessage != "") { Assert.Fail(m.ErrorMessage); }
-            if (e1.ErrorMessage != "") { Assert.Fail(e1.ErrorMessage); }
-            if (e2.ErrorMessage != "") { Assert.Fail(e2.ErrorMessage); }
-            Assert.AreEqual(EхpectedSalaryManager, ActualSalaryManager);
+            TestErrorMessage = m.ErrorMessage + e1.ErrorMessage + e2.ErrorMessage;
+            if (TestErrorMessage != "") { 
+                Assert.Fail(TestErrorMessage); 
+            }else{   
+                Assert.AreEqual(EхpectedSalaryManager, ActualSalaryManager);
+            }            
         }
         [TestMethod]        
         public void TestSalesSalaryWithoutSubordinates()
         {
             // arrange
-            string Name = "Sales Without Subordinates";
             decimal BaseSalary = 400M;
             DateTime DateJoined = new DateTime(2015, 1, 1);
             decimal EхpectedSalary = 416M;
 
             //act
 
-            Sales s = new Sales(Name, BaseSalary, DateJoined);
+            Sales s = new Sales("Sales Without Subordinates", BaseSalary, DateJoined);
             
             decimal ActualSalary = s.Salary;
 
             // assert
-            if (s.ErrorMessage != "") { Assert.Fail(s.ErrorMessage); }
-            Assert.AreEqual(EхpectedSalary, ActualSalary);
+            if (s.ErrorMessage != "") { 
+                Assert.Fail(s.ErrorMessage);
+            }
+            else { 
+                Assert.AreEqual(EхpectedSalary, ActualSalary);
+            }
         }        
         [TestMethod]
         public void TestSalesLongWorkSalaryWitoutSubordinates()
         {
             // arrange
-            string Name = "Sales Long Work";
             decimal BaseSalary = 400M;
             DateTime DateJoined = new DateTime(1975, 1, 1);
             decimal EхpectedSalary = 540M;
 
             //act
 
-            Sales s = new Sales(Name, BaseSalary, DateJoined);
+            Sales s = new Sales("Sales Long Work", BaseSalary, DateJoined);
             decimal ActualSalary = s.Salary;
 
             // assert
-            if (s.ErrorMessage != "") { Assert.Fail(s.ErrorMessage); }
-            Assert.AreEqual(EхpectedSalary, ActualSalary);
+            if (s.ErrorMessage != "") { 
+                Assert.Fail(s.ErrorMessage);
+            }
+            else { 
+                Assert.AreEqual(EхpectedSalary, ActualSalary);
+            }
         }
         [TestMethod]
         public void TestSalesSalaryWithSubordinates()
@@ -151,25 +177,30 @@ namespace TaskTest
             decimal BaseSalaryEmployee2 = 160M;
             decimal BaseSalary = 400M;
             DateTime DateJoined = new DateTime(2015, 1, 1);
-            string Name = "Sales With Subordinates";
-            Manager m = new Manager(Name, BaseSalaryManager, DateJoined);
-            Employee e1 = new Employee(Name, BaseSalaryEmployee1, DateJoined);
+            string TestErrorMessage ="";
+            Manager m = new Manager("Manager", BaseSalaryManager, DateJoined);
+            Employee e1 = new Employee("Employee 1", BaseSalaryEmployee1, DateJoined);
             e1.Supervisor = m;
-            Employee e2 = new Employee(Name, BaseSalaryEmployee2, DateJoined);
+            Employee e2 = new Employee("Employee 2", BaseSalaryEmployee2, DateJoined);
             e2.Supervisor = m;
             decimal EхpectedSalary = 417.68M;
 
             //act
 
-            Sales s = new Sales(Name, BaseSalary, DateJoined);
+            Sales s = new Sales("Sales", BaseSalary, DateJoined);
             m.Supervisor = s;
             decimal ActualSalary = s.Salary;
 
             // assert
-            if (m.ErrorMessage != "") { Assert.Fail(m.ErrorMessage); }
-            if (e1.ErrorMessage != "") { Assert.Fail(e1.ErrorMessage); }
-            if (e2.ErrorMessage != "") { Assert.Fail(e2.ErrorMessage); }
-            Assert.AreEqual(EхpectedSalary, ActualSalary);
+            TestErrorMessage = m.ErrorMessage + e1.ErrorMessage + e2.ErrorMessage+s.ErrorMessage;
+            if (TestErrorMessage != "") {
+                Assert.Fail(TestErrorMessage);
+            }
+            else
+            {
+                Assert.AreEqual(EхpectedSalary, ActualSalary);
+            }
+            
         }
     }    
 }
